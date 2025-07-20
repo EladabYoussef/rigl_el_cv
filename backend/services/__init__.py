0dada70d76ea92b9c7c5b6013fa646cf45abe709
youssphinx <youssefladeb@gmail.com>
2025-07-20 01:42:38 +0100

    it werks

---
+++ backend/services/__init__.py
@@ -0,0 +1,1 @@
+from .parser import text_from_pdf
--- backend/services/ner.py
+++ backend/services/ner.py
@@ -3,6 +3,59 @@ nltk.download('stopwords')
 import spacy
 import re
 
-def entity_recognizer(text: str) -> dict:
-    pass
+def extract_entities(text, nlp):
+    entities = {
+        "NAME":[],
+        "EMAIL":[],
+        "LINKEDIN":[],
+        "PHONE":[],
+        "GITHUB":[],
+        "SKILLS":[],
+        "EXPERIENCE": [],
+    }
 
+    doc = nlp(text)
+    for ent in doc.ents:
+        if ent.label_ == "PER":
+            entities["NAME"].append(ent.text)
+            break
+    try:
+        email = re.findall(r"[\w\.-]+@[\w\.-]+", text)[0]
+    except:
+        email = ""
+    try:
+        github = re.findall(r'(https?:\/\/)?(www\.)?github\.com\/[A-Za-z0-9_.-]+', text)[0]
+    except:
+        github = ""
+    try:
+        linkedin = re.findall(r'(https?:\/\/)?(www\.)?linkedin\.com\/in\/[A-Za-z0-9_-]+', text)[0]
+    except:
+        linkedin = ""
+    text = text.lower()
+    doc = nlp(text)
+    for ent in doc.ents:
+        if ent.label_ == "SKILL" and (ent.text not in entities["SKILLS"]):
+            entities["SKILLS"].append(ent.text)
+    experiences= re.findall(r"\d{4} ?\-\ ?\d{4}|\d{4}\ ?to\ ?\d{4}", text)
+    total_experience = 0
+    for experience in experiences:
+        start, end = experience.replace(" ", "").replace("to", "-").split("-")
+        start, end = int(start), int(end)
+        total_experience += end-start
+    if total_experience > 0:
+        entities["EXPERIENCE"].append(str(total_experience)+" years")
+    return entities
+
+
+if __name__ == "__main__":
+    import sys
+    pdf_path = sys.argv[1]
+    model_path = sys.argv[2]
+    from .parser import text_from_pdf
+    nlp = spacy.load(model_path)
+    with open(pdf_path, "rb") as pdf:
+        bytes_ = pdf.read()
+    text= text_from_pdf(bytes_)
+    entities = extract_entities(text, nlp)
+    for key, value in entities.items():
+        print(key, ":", value)
